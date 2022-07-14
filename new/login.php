1d7e31ef77a247401793cbb80581a49079660f99
samuelidoo45 <samuelaidoo45@gmail.com>
2022-07-14 12:43:52 +0000

    update

---
+++ new/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	  
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+</head>
+<body>
+	<form class = "container">
+		<div>
+			<label class="login">
+				<h1>Login</h1>
+			</label>
+			<p>Don't have an account ?<a href="signup.php">Sign up</a></p>
+		</div>
+			<label>Email</label>
+		<div class="input-group mb-3">
+          <input type="email" class="form-control" name = "email"  required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+		
+		<label>Password</label><br>
+		<div class="input-group mb-3">
+		<input type="password"  class="form-control"  required id="password">	
+		<div class="input-group-append">
+            <div class="input-group-text">
+		<span class="far fa-eye" id="togglePassword" style=" cursor: pointer;"></span>
+		</div>
+          </div>
+        </div>
+				
+		<button type="submit" class="btn btn-danger btn-block" >LOGIN</button>
+	</form>
+<script type="text/javascript">
+	const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#password');
+
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+</script>
+</body>
+</html>
---
+++ new/sidebar/index.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Sidebar</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+		
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+		
+		<div class="wrapper d-flex align-items-stretch">
+			<nav id="sidebar">
+				<div class="custom-menu">
+					<button type="button" id="sidebarCollapse" class="btn btn-primary">
+	          <i class="fa fa-bars"></i>
+	          <span class="sr-only"></span>
+	        </button>
+        </div>
+				<div class="p-4">
+		  		<h1 style="color: white;">Main</h1>
+	        <ul class="list-unstyled components mb-5">
+	          <li>
+	            <a href="#"><span class="fa fa-dashboard mr-3"></span> Dashboard</a>
+	          </li>
+	          <li>
+	              <a href="#"><span class="fa fa-user mr-3"></span> Users</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-upload mr-3"></span> Upload Questions</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-undo mr-3"></span> Update Questions</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-trash mr-3"></span> Delete Questions</a>
+	          </li>
+	          <li>
+              <a href="#"><span class="fa fa-pencil mr-3"></span> Create Admin</a>
+	          </li>
+	        </ul>			
+    	</nav>
+
+        <!-- Page Content  -->
+      <div id="content" class="p-4 p-md-5 pt-5">
+        <nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav" style="width: 100%">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        
+        
+     <div style="flex: 2;"></div>
+     
+     	<li class="nav-item">
+			   <div class="input-group">
+			  <div class="form-outline">
+			    <input type="search" class="form-control"  placeholder="search" />
+			  </div>
+			  <button type="button" >
+			    <i class="fa fa-search"></i>
+			  </button>
+			</div>
+     </li>
+     	<li class="nav-item ">
+        	<img src="/contact_crud2/images/img3.jpg" width="30" height="30">
+          <a class="navbar-brand" href="#">Admin</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+<nav class="navbar navbar-light bg-light">
+  <div class="container">
+    
+  </div>
+</nav>
+<!-- cards -->
+<div style="display: flex;">
+<div class="card" style="width: 12rem; margin: 3rem;">
+  <div class="card-body" >
+    <img src="images/user.jpg" class="card-img-top" ><p>800</p>
+    <h5 class="card-title" style="color: green; ">Users</h5>
+  </div>
+</div>
+<div class="card" style="width: 12rem;  margin: 3rem;">
+  <div class="card-body">
+    <img src="images/user.jpg" class="card-img-top" ><p>800</p>
+    <h5 class="card-title" style="color: green; ">Department</h5>
+    
+  </div>
+</div>
+<div class="card" style="width: 12rem;  margin: 3rem;">
+  <div class="card-body">
+    <img src="images/user.jpg" class="card-img-top" ><p>800</p>
+    <h5 class="card-title" style="color: green; ">Questions</h5>
+    
+  </div>
+</div>
+
+</div>
+
+
+         </div>
+		</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
